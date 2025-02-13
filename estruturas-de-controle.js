aa1eacc538da68d2382e20bc054a408943fccbcc
joaogananca26 <joaogananca26@gmail.com>
2025-02-13 20:37:22 -0300

    Exercício 1 de Estruturas de Controle feito!

---
+++ estruturas-de-controle.js
@@ -0,0 +1,11 @@
+// Exercício 1
+
+let numero = -10;
+
+if (numero > 0) {
+    console.log("O número é positivo.");
+} else if (numero < 0) {
+    console.log("O número é negativo.");
+} else {
+    console.log("O número é zero.");
+}
