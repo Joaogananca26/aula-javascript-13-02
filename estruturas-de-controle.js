57251908f8300a063bd618ad62c879d4d7054fbc
joaogananca26 <joaogananca26@gmail.com>
2025-02-13 20:47:47 -0300

    Exercício 1C de Estruturas de Controle feito!

--- estruturas-de-controle.js
+++ estruturas-de-controle.js
@@ -15,3 +15,15 @@ if (numero > 0) {
 for (let i = 0; i < 11; i++){
     console.log(i)
 }
+
+// Exercício C
+let soma = 0;
+let i = 1;
+
+
+while (i <= 5) {
+    soma += i; 
+    i++; 
+}
+
+console.log("A soma dos números de 1 a 5 é: " + soma);
