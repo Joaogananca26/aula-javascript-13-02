454fe4dcd9593af5e33a2f35c70139c86ad133eb
joaogananca26 <joaogananca26@gmail.com>
2025-02-13 20:13:20 -0300

    Exercício 2 de Variáveis e Tipos de Dados feito!

--- variaveis-e-tipos-de-dados.js
+++ variaveis-e-tipos-de-dados.js
@@ -14,3 +14,21 @@ console.log(usuario)
 /* Null é quando você cria um variável com o valor "null" e ela só trocará o valor quando aplicar um valor válido a ela, já o undefined
 é quando você define uma variável e não da um valor a ela.
 */
+
+// Exercício 2
+
+let valorNulo = null;  
+let valorIndefinido;   
+
+// Condicional para verificar se as variáveis são null ou undefined
+if (valorNulo === null) {
+    console.log("valorNulo é null");
+} else if (valorNulo === undefined) {
+    console.log("valorNulo é undefined");
+}
+
+if (valorIndefinido === undefined) {
+    console.log("valorIndefinido é undefined");
+} else if (valorIndefinido === null) {
+    console.log("valorIndefinido é null");
+}
