fd12f4085ff0c439461bf4744ab134749f627fa1
joaogananca26 <joaogananca26@gmail.com>
2025-02-13 20:12:40 -0300

    Exercício 1 de Variáveis e Tipos de Dados feito!

---
+++ variaveis-e-tipos-de-dados.js
@@ -0,0 +1,16 @@
+// Exercício 1
+
+let nome = "João Henrique Murilla Ganança";
+let idade = 19;
+let aprovado = false;
+let notas = [6.0, 8.0, 9.5]
+let usuario = {email: "joaogananca26@gmail.com", senha: "joaozinho"}
+console.log(nome)
+console.log(idade)  
+console.log(aprovado)
+console.log(notas)
+console.log(usuario)
+
+/* Null é quando você cria um variável com o valor "null" e ela só trocará o valor quando aplicar um valor válido a ela, já o undefined
+é quando você define uma variável e não da um valor a ela.
+*/
