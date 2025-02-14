29ee87469c259f851adeb2678cc8724f0e973985
joaogananca26 <joaogananca26@gmail.com>
2025-02-13 21:48:31 -0300

    Exercício 2 de Estruturas de Controle feitos e todos os outros exercícios integrados ao index.html

--- estruturas-de-controle.js
+++ estruturas-de-controle.js
@@ -1,7 +1,9 @@
 // Exercício 1
 
 // Exercício A
-let numero = -10;
+let numero;
+
+numero = prompt("Escolha um número e irei te falar se é positivo, negativo ou se ele é zero!")
 
 if (numero > 0) {
     console.log("O número é positivo.");
@@ -27,3 +29,27 @@ while (i <= 5) {
 }
 
 console.log("A soma dos números de 1 a 5 é: " + soma);
+
+// Exercício 2
+
+let opcao;
+
+do {
+    // Exibe o menu para o usuário
+    opcao = prompt("Escolha uma opção:\nA - Opção A\nB - Opção B\nS - Sair");
+
+    // Usando switch-case para tratar as opções
+    switch(opcao) {
+        case 'A':
+            console.log("Opção A selecionada");
+            break;
+        case 'B':
+            console.log("Opção B selecionada");
+            break;
+        case 'S':
+            console.log("Saindo...");
+            break;
+        default:
+            console.log("Opção inválida");
+    }
+} while (opcao !== 'S');
--- variaveis-e-tipos-de-dados.js
+++ variaveis-e-tipos-de-dados.js
@@ -5,11 +5,7 @@ let idade = 19;
 let aprovado = false;
 let notas = [6.0, 8.0, 9.5]
 let usuario = {email: "joaogananca26@gmail.com", senha: "joaozinho"}
-console.log(nome)
-console.log(idade)  
-console.log(aprovado)
-console.log(notas)
-console.log(usuario)
+console.log(nome, idade, aprovado, notas, usuario)
 
 /* Null é quando você cria um variável com o valor "null" e ela só trocará o valor quando aplicar um valor válido a ela, já o undefined
 é quando você define uma variável e não da um valor a ela.
